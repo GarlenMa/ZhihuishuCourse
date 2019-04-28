8469fc6f2ea429115092e38cdfba8251eaeeeaaa
Garlen <40255954+GarlenMa@users.noreply.github.com>
2019-04-28 21:20:36 +0800

    Update ZhihuishuCourse.js

--- ZhihuishuCourse.js
+++ ZhihuishuCourse.js
@@ -1,24 +1,24 @@
 ﻿setInterval(function () {
-    //设置清晰度为流畅
+    // 设置清晰度为流畅
     if ($('.line1gq').hasClass('active')) {
         $('.line1bq').click();
     }
-    //设置1.5倍速
+    // 设置1.5倍速
     $('.speedTab15').click();
-    //静音
+    // 静音
     setTimeout(function () {
         if (!$('.volumeBox').hasClass('volumeNone')) {
             $('.volumeIcon').click();
         }
     }, 500);
-    //关闭弹题
+    // 关闭弹题
     if ($('.isExamFinish')) {
         $('.isExamFinish').val('0');
         setTimeout(function () {
             $('.popboxes_close.tmui_txt_hidd').click();
         }, 500);
     }
-    //自动播放下一集
+    // 自动播放下一集
     if (($('.currentTime').text() == $('.duration').text()) && $('.currentTime').text() != '00:00:00') {
         $('#nextBtn').click();
     }
